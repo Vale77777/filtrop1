6640701407880d598b5da522ba79be9b955489b8
Vale77777 <133288728+Vale77777@users.noreply.github.com>
2023-12-26 18:50:18 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload() {
+
+}
+
+function setup() {
+  canvas= createCanvas(300,300);
+  canvas.center();
+}
+
+function modelLoaded() {
+
+}
+
+function gotPoses(results)
+{
+
+  
+}
+
+function draw() {
+  
+ 
+}
+
+function take_snapshot(){    
+    save('myFilterImage.png')
+}
